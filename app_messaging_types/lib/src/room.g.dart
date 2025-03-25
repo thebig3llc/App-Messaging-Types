294bc819720d629ea322e7065955bcb7d3a4759f
Justin <goshornjustin@gmail.com>
2025-03-25 13:22:30 -0400

    added package files

---
+++ app_messaging_types/lib/src/room.g.dart
@@ -0,0 +1,43 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'room.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Room _$RoomFromJson(Map<String, dynamic> json) => Room(
+  createdAt: (json['createdAt'] as num?)?.toInt(),
+  id: json['id'] as String,
+  imageUrl: json['imageUrl'] as String?,
+  lastMessages:
+      (json['lastMessages'] as List<dynamic>?)
+          ?.map((e) => Message.fromJson(e as Map<String, dynamic>))
+          .toList(),
+  metadata: json['metadata'] as Map<String, dynamic>?,
+  name: json['name'] as String?,
+  type: $enumDecodeNullable(_$RoomTypeEnumMap, json['type']),
+  updatedAt: (json['updatedAt'] as num?)?.toInt(),
+  users:
+      (json['users'] as List<dynamic>)
+          .map((e) => ChatUser.fromJson(e as Map<String, dynamic>))
+          .toList(),
+);
+
+Map<String, dynamic> _$RoomToJson(Room instance) => <String, dynamic>{
+  'createdAt': instance.createdAt,
+  'id': instance.id,
+  'imageUrl': instance.imageUrl,
+  'lastMessages': instance.lastMessages,
+  'metadata': instance.metadata,
+  'name': instance.name,
+  'type': _$RoomTypeEnumMap[instance.type],
+  'updatedAt': instance.updatedAt,
+  'users': instance.users,
+};
+
+const _$RoomTypeEnumMap = {
+  RoomType.channel: 'channel',
+  RoomType.direct: 'direct',
+  RoomType.group: 'group',
+};
---
+++ app_messaging_types/test/app_messaging_types_test.dart
@@ -0,0 +1,5 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:app_messaging_types/app_messaging_types.dart';
+
+void main() {}
